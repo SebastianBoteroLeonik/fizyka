4aed805c0a42bd992c615d1700093c9e0812ae6b
sedavbotero <sdboterol@protonmail.com>
2025-03-25 17:07:28 +0100

    Poprawki w wykresach

--- lab2/obliczenia.R
+++ lab2/obliczenia.R
@@ -29,8 +29,15 @@ df |>
   geom_point() +
   geom_errorbarh(aes(xmin = UH - uBUH, xmax = UH + uBUH)) +
   geom_errorbar(aes(ymin = ln_I - u_ln_I, ymax = ln_I + u_ln_I)) +
-  geom_smooth(method = "lm", se = FALSE)
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(
+    x = TeX("$U_H$"),
+    y = TeX("$\\ln{\\frac{I_a}{I_{a,0}}}$"),
+    color = "seria"
+  )
 
+ggsave("lab2/dopasowanie.pdf", device = cairo_pdf,
+       width = 10, height = 6)
 
 s1 |>
   arrange(UH) |>
@@ -52,7 +59,7 @@ s1 |>
   geom_line() +
   labs(
     y = TeX("$ I_a$ $(\\mu A)$"),
-    x = TeX("$\\sqrt{U_h}$ $(\\sqrt{V})$"),
+    x = TeX("$\\sqrt{U_H}$ $(\\sqrt{V})$"),
     title = "Wykres zależności prądu anodowego\nod pierwiastku napięcia hamującego"
   ) +
   theme(title = element_text(family = "ArialMT"))
